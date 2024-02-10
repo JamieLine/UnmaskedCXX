1bdb8b204351feea0f3333e80a556d1586520bb6
Jamie Line <60134692+JamieLine@users.noreply.github.com>
2024-02-10 19:30:07 +0000

    Test specifications can add additional files to the resulting test source (#19)
    Additional includes are now accepted

--- TestCreator/Consts/TestMarkers.h
+++ TestCreator/Consts/TestMarkers.h
@@ -8,5 +8,6 @@ const std::string ALWAYS_RETURN_VALUE_TEST_MARKER =
 const std::string STABILISING_TEST_MARKER = "UnmaskedStabilisingSetTest";
 const std::string SET_PARAMETER_MARKER = "UnmaskedSetParameter";
 const std::string SET_TEMP_PARAMETER_MARKER = "UnmaskedSetTempParameter";
+const std::string INCLUDE_FILE_MARKER = "UnmaskedIncludeFile";
 
 #endif /* TESTCREATOR_CONSTS_TESTMARKERS_H */
--- TestCreator/Parsers/CreateAlwaysReturnValueTest.cpp
+++ TestCreator/Parsers/CreateAlwaysReturnValueTest.cpp
@@ -18,11 +18,12 @@
 #include "../Structs/FixedArgument.h"
 #include "../Structs/TestSpecification.h"
 
-auto CreateAlwaysReturnValueTest(std::vector<std::string>::iterator& FirstToken,
-                                 GeneratorParameterStoreSeed Params,
-                                 Filepath& TestDefinitionPath,
-                                 const std::string& GeneratedFunctionName,
-                                 std::size_t NumTestsToRun) -> std::string {
+auto CreateAlwaysReturnValueTest(
+    std::vector<std::string>::iterator& FirstToken,
+    GeneratorParameterStoreSeed Params, Filepath& TestDefinitionPath,
+    const std::vector<std::string>& AdditionalIncludes,
+    const std::string& GeneratedFunctionName, std::size_t NumTestsToRun)
+    -> std::string {
   // Creates a test from Tokens extracted from input similar to the following:
   // `UnmaskedAlwaysReturnsValueTest(std::function<int(int, int)>(&AddInts), 0,
   // 0, 0);`
@@ -208,6 +209,11 @@ auto CreateAlwaysReturnValueTest(std::vector<std::string>::iterator& FirstToken,
   // Set the function name
   TestSource =
       ReplaceAllInString(TestSource, "TEST_FN_NAME", GeneratedFunctionName);
+
+  // Add additional includes
+  TestSource =
+      ReplaceAllInString(TestSource, "ADDITIONAL_INCLUDES",
+                         JoinVectorOfStrings(AdditionalIncludes, "\n"));
 
   // Fix the return type and argument types
   TestSource = ReplaceAllInString(TestSource, "RETURN_TYPE", ReturnType);
